f9bd048f943627b055224244c9a4921361b29c95
Chris Barker <PythonCHB@gmail.com>
2018-01-23 16:56:33 -0800

    added context manager exercise

--- source/solutions/trapezoidal/trapz.py
+++ source/solutions/trapezoidal/trapz.py
@@ -11,57 +11,56 @@ def quadratic(x, A=0, B=0, C=0):
     return A * x**2 + B * x + C
 
 
-# this version returns a list
-def frange(a, b, n=100):
-    """
-    kind of like a floating point range function
+# # this version returns a list
+# def frange(a, b, n=100):
+#     """
+#     kind of like a floating point range function
 
-    :param a: the start point
-    :param b: the end point
-    :param n: the number of intervals you want.
+#     :param a: the start point
+#     :param b: the end point
+#     :param n: the number of intervals you want.
 
-    :returns: a sequence of floating point numbers, evenly spaced between
-    a and b
+#     :returns: a sequence of floating point numbers, evenly spaced between
+#     a and b
 
-    result[0] == a
-    result[-1] == b
-    len(result) == n+1
+#     result[0] == a
+#     result[-1] == b
+#     len(result) == n+1
 
-    n specifies the number of intervals, so you get a nice delta. i.e.
-    frange(1,10,100) == [1.0, 1.1, 1.2, ..., 9.8, 9.9, 10.0]
+#     n specifies the number of intervals, so you get a nice delta. i.e.
+#     frange(1,10,100) == [1.0, 1.1, 1.2, ..., 9.8, 9.9, 10.0]
 
-    """
-    delta = (float(b) - a) / n
-    return [a + i * delta for i in range(n + 1)]
+#     """
+#     delta = (float(b) - a) / n
+#     return [a + i * delta for i in range(n + 1)]
 
 
-# this version returns a generator
-def frange(a, b, n=100):
-    """
-    kind of like a floating point range function
+# # this version returns a generator
+# def frange(a, b, n=100):
+#     """
+#     kind of like a floating point range function
 
-    :param a: the start point
-    :param b: the end point
-    :param n: the number of intervals you want.
+#     :param a: the start point
+#     :param b: the end point
+#     :param n: the number of intervals you want.
 
-    :returns: a iterator of floating point numbers, evenly spaced between
-    a and b
+#     :returns: a iterator of floating point numbers, evenly spaced between
+#     a and b
 
-    The first value is a
-    The last value is b
-    The total number of values is n+1
+#     The first value is a
+#     The last value is b
+#     The total number of values is n+1
 
-    n specifies the number of intervals, so you get a nice delta. i.e.
-    list(frange(1,10,100)) == [1.0, 1.1, 1.2, ..., 9.8, 9.9, 10.0]
+#     n specifies the number of intervals, so you get a nice delta. i.e.
+#     list(frange(1,10,100)) == [1.0, 1.1, 1.2, ..., 9.8, 9.9, 10.0]
 
-    """
-    delta = (float(b) - a) / int(n)
-    for i in range(n):
-        yield a + i * delta
-    yield b
+#     """
+#     delta = (float(b) - a) / int(n)
+#     for i in range(n):
+#         yield a + i * delta
+#     yield b
 
-# the really fancy way to do it, so it can be sliced!
-
+from frange import frange
 
 
 
@@ -81,10 +80,11 @@ def trapz(fun, a, b, *args, **kwargs):
     """
     # compute the range
     n = 100  # hard code that for now
-    vals = frange(a, b, n)
+    # vals = iter(frange(a, b, n))
 
-    next(vals)
-    s = sum([fun(next(vals), *args, **kwargs) for i in range(n - 1)])
+    # next(vals)
+    # s = sum([fun(next(vals), *args, **kwargs) for i in range(n - 1)])
+    s = sum([fun(val, *args, **kwargs) for val in frange(a, b, n)[1:-1]])
     s += (fun(a, *args, **kwargs) + fun(b, *args, **kwargs)) / 2
     s *= (b - a) / n
 
