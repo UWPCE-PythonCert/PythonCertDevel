96b504a363318fd75f725cdb94e0bf7d7c66d264
Chris Barker <PythonCHB@gmail.com>
2018-01-28 21:24:24 -0800

    added context manager solution

--- source/solutions/context_managers/nifty_context.py
+++ source/solutions/context_managers/nifty_context.py
@@ -4,6 +4,7 @@
 A couple nifty context managers
 """
 
+
 class Raises:
     def __init__(self, *args):
         print("initializing:", args)
@@ -11,18 +12,24 @@ class Raises:
 
     def __enter__(self):
         """nothing to be done here."""
-        print("in enter")
         pass
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         """ Here's where we check the exceptions """
-        print("exiting:", exc_type)
         if exc_type in self.exceptions:
             # tests pass
             return True
         else:
             if exc_type is None:
-                msg = "No error was raised -- expected one of {}".format(self.exceptions)
+                msg = "No error was raised -- expected one of {}".format(
+                      [e.__name__ for e in self.exceptions])
             else:
-                msg = "{} raised -- expected {}".format(exc_type, self.exceptions)
+                if len(self.exceptions) == 1:
+                    msg = "{} raised -- expected {}".format(exc_type.__name__,
+                           self.exceptions[0].__name__)
+                else:
+                    msg = "{} raised -- expected one of: {}".format(
+                           exc_type.__name__,
+                           [e.__name__ for e in self.exceptions])
+
             assert False, msg
