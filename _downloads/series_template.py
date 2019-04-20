94fa786ef7aa9e29862acee08399171379d00c29
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-20 21:42:21 +0000

    Deploy UWPCE-PythonCert/PythonCertDevel to github.com/UWPCE-PythonCert/PythonCertDevel.git:gh-pages

--- _downloads/series_template.py
+++ _downloads/series_template.py
@@ -21,11 +21,16 @@ def sum_series(n, n0=0, n1=1):
 
     :param n0=0: value of zeroth element in the series
     :param n1=1: value of first element in the series
-    
+
     This function should generalize the fibonacci() and the lucas(),
     so that this function works for any first two numbers for a sum series.
     Once generalized that way, sum_series(n, 0, 1) should be equivalent to fibonacci(n).
     And sum_series(n, 2, 1) should be equivalent to lucas(n).
+
+    sum_series(n, 3, 2) should generate antoehr series with no specific name
+
+    The defaults are set to 0, 1, so if you don't pass in any values, you'll
+    get the fibonacci sercies
     """
     pass
 
@@ -45,9 +50,19 @@ if __name__ == "__main__":
 
     assert lucas(4) == 7
 
+    # test that sum_series matches fibonacci
     assert sum_series(5) == fibonacci(5)
+    assert sum_series(7, 0, 1) == fibonacci(7)
 
     # test if sum_series matched lucas
     assert sum_series(5, 2, 1) == lucas(5)
+
+    # test if sum_series works for arbitrary initial values
+    assert sum_series(0, 3, 2) == 3
+    assert sum_series(1, 3, 2) == 2
+    assert sum_series(2, 3, 2) == 5
+    assert sum_series(3, 3, 2) == 7
+    assert sum_series(4, 3, 2) == 12
+    assert sum_series(5, 3, 2) == 19
 
     print("tests passed")
